04c32bd3a1c887981f534b462295987c5c88f726
bayyoo <148179524+bayyoo@users.noreply.github.com>
2023-12-03 21:16:02 +0700

    config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
 $dbhost = 'localhost';
-$dbuser = 'id21443593_dane';
-$dbpass = 'Zidaneotwitkonsultan1!';
-$dbname = 'id21443593_bukutamu';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'nukutamu';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
 mysqli_select_db($conn, $dbname);
 ?>
